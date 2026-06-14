5adc0f8a03fc1645e0ab629585bfe43cd0792c37
Aloysious Kok <aloysiousksk@gmail.com>
2026-06-15 02:37:25 +0800

    Proxy inventory admin at /admin

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/admin",
+          destination: "https://tcginvent.vercel.app/admin",
+        },
+        {
+          source: "/admin/:path*",
+          destination: "https://tcginvent.vercel.app/admin/:path*",
+        },
+      ],
+    };
+  },
+};
 
 module.exports = nextConfig;
